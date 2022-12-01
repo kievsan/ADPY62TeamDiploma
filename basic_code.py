cc8bc8f955cd34ce0b4f59eb3e0879e136377c92
Sergey Kievskiy <knhel7@mail.ru>
2022-12-01 07:42:01 +0300

    edit README.md

--- basic_code.py
+++ basic_code.py
@@ -3,22 +3,32 @@ from random import randrange
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 
-token = input('Token: ')
-
-vk = vk_api.VkApi(token=token)
-longpoll = VkLongPoll(vk)
+# token = input('Token: ')
+with open('token.txt', 'r') as file_object:
+    token = file_object.read().strip()
+vk = vk_api.VkApi(token=token)  # Авторизуемся как сообщество
+longpoll = VkLongPoll(vk)  # Работа с сообщениями
 
 
 def write_msg(user_id, message):
-    vk.method('messages.send', {'user_id': user_id, 'message': message,  'random_id': randrange(10 ** 7),})
+    """"
+    получает id пользователя ВК <user_id>, и сообщение ему
+    """
+    vk.method('messages.send',
+              {'user_id': user_id, 'message': message, 'random_id': randrange(10 ** 7), })
 
 
-for event in longpoll.listen():
-    if event.type == VkEventType.MESSAGE_NEW:
-
-        if event.to_me:
-            request = event.text
-
+for event in longpoll.listen():  # Основной цикл событий
+    """"
+    мы циклически будем проверять на наличие event-ов. А получить тип event-а сможем с помощью event.type.
+    После этого получив сообщение от пользователя сможем отправить ему соответствующее письмо с помощью 
+    уже созданной функции write_msg.
+    Cоздан очень простой бот в ВК с такой же простой реализацией. А логику бота можно программировать как душе угодно.
+    """
+    if event.type == VkEventType.MESSAGE_NEW:  # Если пришло новое сообщение
+        if event.to_me:  # Если оно имеет метку для бота
+            request = event.text    # Сообщение от пользователя
+            # логика ответа:
             if request == "привет":
                 write_msg(event.user_id, f"Хай, {event.user_id}")
             elif request == "пока":
