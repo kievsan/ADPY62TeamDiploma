2e5da8b707739bf821abe99c23368e18b6c18979
Sergey Kievskiy <knhel7@mail.ru>
2022-12-17 17:49:32 +0300

    checkers was created: search mode

---
+++ vk_tools/advanced_checker.py
@@ -0,0 +1,15 @@
+#
+
+from vk_tools.checker import VkUserChecker
+from vk_api.vk_api import VkApiMethod
+
+
+class AdvancedChecker(VkUserChecker):
+    _skill = 'A matchmaker advanced Search Engine'
+
+    def __init__(self, client_id: str, api_methods: VkApiMethod, search_filter: dict = {}):
+        super(AdvancedChecker, self).__init__(client_id, api_methods, search_filter, self._skill)
+
+    def is_advisable_user(self, vk_id: str) -> bool:
+        super(AdvancedChecker, self).is_advisable_user(vk_id)
+        return False
---
+++ vk_tools/interest_checker.py
@@ -0,0 +1,15 @@
+#
+
+from vk_tools.checker import VkUserChecker
+from vk_api.vk_api import VkApiMethod
+
+
+class InterestChecker(VkUserChecker):
+    _skill = 'An interest matchmaker Search Engine'
+
+    def __init__(self, client_id: str, api_methods: VkApiMethod, search_filter: dict = {}):
+        super(InterestChecker, self).__init__(client_id, api_methods, search_filter, self._skill)
+
+    def is_advisable_user(self, vk_id: str) -> bool:
+        super(InterestChecker, self).is_advisable_user(vk_id)
+        return False
